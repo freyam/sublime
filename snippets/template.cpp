cc9faf26a519f032530121fce1b94337a1c6c814
Freyam Mehta <62539811+freyam@users.noreply.github.com>
2020-06-28 11:03:16 +0400

    Update and rename f to template.cpp

---
+++ snippets/template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define f first
+#define s second
+#define ins insert
+#define eb emplace_back
+#define endl '\n'
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define vl vector<ll>
+#define ii pair<int,int>
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+#define tc int t; cin >> t; while (t--)
+#define sz(c) (int) (c).size()
+#define trav(x,c) for(auto &x : c)
+#define all(c) (c).begin(), (c).end()
+#define rall(c) (c).rbegin(), (c).rend()
+#define _for(i,a,n) for(int i = a ; i < n; ++i)
+#define _rfor(i,a,n) for(int i = n-1 ; i >= a; --i)
+#define db(x) cout << #x << ": " << x << endl;
+#define _db(c) trav(x,c) cout << x << " "; cout << endl;
+#define godspeed ios::sync_with_stdio(0); cin.tie(0);
+
+int main() {
+    godspeed;
+    
+}
