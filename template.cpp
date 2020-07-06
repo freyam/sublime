1c3765eb3d4ccdca261f33a2def62b870ce6e7ba
Freyam Mehta <62539811+freyam@users.noreply.github.com>
2020-07-06 11:13:27 +0400

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -8,21 +8,20 @@ using namespace std;
 #define endl '\n'
 #define ll long long
 #define ld long double
-#define vi vector<int>
 #define vl vector<ll>
-#define ii pair<int,int>
-#define tc int t; cin >> t; while (t--)
-#define sz(c) (int) (c).size()
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+#define tc ll t; cin >> t; while (t--)
+#define sz(c) (ll) (c).size()
 #define trav(x,c) for(auto &x : c)
 #define all(c) (c).begin(), (c).end()
 #define rall(c) (c).rbegin(), (c).rend()
-#define _for(i,a,n) for(int i = a ; i < n; ++i)
-#define _rfor(i,a,n) for(int i = n-1 ; i >= a; --i)
+#define _for(i,a,n) for(ll i = a ; i < n; ++i)
+#define _rfor(i,a,n) for(ll i = n-1 ; i >= a; --i)
 #define db(x) cout << #x << ": " << x << endl;
 #define _db(c) trav(x,c) cout << x << " "; cout << endl;
 #define godspeed ios::sync_with_stdio(0); cin.tie(0);
 template<typename... T> void read(T&... x) {((cin >> x),...);}
-template<typename... T> void print(T&&... x) {((cout << x << " "),...);}
 
 int main() {
     godspeed;
