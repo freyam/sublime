3cb8c6e807f98b8087a6d9984f2722801cb4cd3f
Freyam Mehta <62539811+freyam@users.noreply.github.com>
2020-07-19 07:53:12 +0400

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -9,18 +9,19 @@ using namespace std;
 #define ll long long
 #define ld long double
 #define vl vector<ll>
-#define no cout << "NO" << endl
-#define yes cout << "YES" << endl
 #define tc ll t; cin >> t; while (t--)
-#define sz(c) (ll) (c).size()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define sz(c) (ll) c.size()
+#define all(c) c.begin(), c.end()
+#define rall(c) c.rbegin(), c.rend()
 #define trav(x,c) for(auto &x : c)
-#define all(c) (c).begin(), (c).end()
-#define rll(c) (c).rbegin(), (c).rend()
-#define _for(i,a,n) for(ll i = a ; i < n; ++i)
-#define _rfor(i,a,n) for(ll i = n-1 ; i >= a; --i)
+#define _for(i,a,n) for(ll i = a ; i < n ; ++i)
+#define _rfor(i,a,n) for(ll i = n-1 ; i >= a ; --i)
 #define dx(x) cout << #x << ": " << x << endl;
 #define dc(c) trav(x,c) cout << x << " "; cout << endl;
-#define godspeed ios::sync_with_stdio(0); cin.tie(0);
+#define dm(m) trav(x,m) cout << x.f << " " << x.s << endl; cout << endl;
+#define godspeed cin.tie(0) -> sync_with_stdio(0);
 
 int main() {
     godspeed;
