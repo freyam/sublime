85685b364acfe6a3a43460ff125975d8e290ec56
Freyam Mehta <62539811+freyam@users.noreply.github.com>
2020-07-23 22:36:32 +0400

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,26 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define f first
-#define s second
+using ll = long long;
+using ld = long double;
+using vl = vector<ll>;
+using ml = map<ll,ll>;
+
+#define ff first
+#define ss second
+#define endl '\n'
 #define ins insert
 #define eb emplace_back
-#define endl '\n'
-#define ll long long
-#define ld long double
-#define vl vector<ll>
-#define tc ll t; cin >> t; while (t--)
-#define yes cout << "YES" << endl
-#define no cout << "NO" << endl
 #define sz(c) (ll) c.size()
-#define all(c) c.begin(), c.end()
-#define rall(c) c.rbegin(), c.rend()
-#define trav(x,c) for(auto &x : c)
-#define _for(i,a,n) for(ll i = a ; i < n ; ++i)
-#define _rfor(i,a,n) for(ll i = n-1 ; i >= a ; --i)
+#define all(c) begin(c), end(c)
+#define rall(c) rbegin(c), rend(c)
+#define itr(x,c) for(auto &x : c)
+#define tc ll t; cin >> t; while (t--)
+#define rep(i,a,n) for(ll i = a ; i < n ; ++i)
+#define per(i,a,n) for(ll i = n-1 ; i >= a ; --i)
 #define dx(x) cout << #x << ": " << x << endl;
-#define dc(c) trav(x,c) cout << x << " "; cout << endl;
-#define dm(m) trav(x,m) cout << x.f << "->" << x.s << endl;
+#define dc(c) itr(x,c) cout << x << " "; cout << endl;
+#define dm(m) itr(x,m) cout << x.f << "->" << x.s << endl;
 #define godspeed cin.tie(0) -> sync_with_stdio(0);
 
 int main() {
