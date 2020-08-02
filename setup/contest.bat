c57783431dfd387dc11feacddb4fb84452cc1ee6
Freyam Mehta <62539811+freyam@users.noreply.github.com>
2020-08-02 18:26:18 +0400

    Update contest.bat

--- setup/contest.bat
+++ setup/contest.bat
@@ -1,28 +1,31 @@
 @echo off
 
-if EXIST contest_a.cpp goto DEL
-if NOT EXIST contest_a.cpp goto NEW
+if EXIST contest_A.cpp goto DEL
+if NOT EXIST contest_A.cpp goto NEW
 
 :NEW
-copy code.cpp "contest_a.cpp"
-copy code.cpp "contest_b.cpp"
-copy code.cpp "contest_c.cpp"
-copy code.cpp "contest_d.cpp"
-copy code.cpp "contest_e.cpp"
-copy code.cpp "contest_f.cpp"
+copy code.cpp contest_A.cpp
+copy code.cpp contest_B.cpp
+copy code.cpp contest_C.cpp
+copy code.cpp contest_D.cpp
+copy code.cpp contest_E.cpp
+copy code.cpp contest_F.cpp
 exit
 
 :DEL
-del contest_a.cpp
-del contest_b.cpp
-del contest_c.cpp
-del contest_d.cpp
-del contest_e.cpp
-del contest_f.cpp
-del contest_a.exe
-del contest_b.exe
-del contest_c.exe
-del contest_d.exe
-del contest_e.exe
-del contest_f.exe
+del contest_A.cpp
+del contest_B.cpp
+del contest_C.cpp
+del contest_D.cpp
+del contest_E.cpp
+del contest_F.cpp
+del contest_A.exe
+del contest_B.exe
+del contest_C.exe
+del contest_D.exe
+del contest_E.exe
+del contest_F.exe
+del input && del output
+.> input
+.> output
 exit
