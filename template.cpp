5ee09a191a8970efc75397555341c94377bd72dd
Freyam Mehta <62539811+freyam@users.noreply.github.com>
2020-08-04 01:02:44 +0400

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-using ld = long double;
-using vl = vector<ll>;
-using ml = map<ll,ll>;
-
 #define ff first
 #define ss second
+#define ll long long
+#define ld long double
+#define vl vector<ll>
+#define ml map<ll,ll>
 #define endl '\n'
 #define ins insert
 #define eb emplace_back
@@ -15,13 +14,10 @@ using ml = map<ll,ll>;
 #define all(c) begin(c), end(c)
 #define rall(c) rbegin(c), rend(c)
 #define itr(x,c) for(auto &x : c)
-#define tc ll t; cin >> t; while (t--)
+#define tc ll t; cin >> t; while(t--)
 #define rep(i,a,n) for(ll i = a ; i < n ; ++i)
 #define per(i,a,n) for(ll i = n-1 ; i >= a ; --i)
-#define dx(x) cout << #x << ": " << x << endl;
-#define dc(c) itr(x,c) cout << x << " "; cout << endl;
-#define dm(m) itr(x,m) cout << x.ff << "->" << x.ss << endl;
-#define godspeed cin.tie(0) -> sync_with_stdio(0);
+#define godspeed cin.tie(0) -> sync_with_stdio(0)
 
 int main() {
     godspeed;
