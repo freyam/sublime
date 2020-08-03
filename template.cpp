d7ca5af1877af9d343384750dcc41003e0baf37d
Freyam Mehta <62539811+freyam@users.noreply.github.com>
2020-08-04 01:43:17 +0400

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define endl '\n'
+#define ins insert
 #define ff first
 #define ss second
 #define ll long long
 #define ld long double
 #define vl vector<ll>
 #define ml map<ll,ll>
-#define endl '\n'
-#define ins insert
 #define eb emplace_back
 #define sz(c) (ll) c.size()
 #define all(c) begin(c), end(c)
