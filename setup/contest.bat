7064532ebc2026af348371db0eb72bf2db064b2b
Freyam Mehta <62539811+freyam@users.noreply.github.com>
2020-08-04 10:50:50 +0400

    Update contest.bat

--- setup/contest.bat
+++ setup/contest.bat
@@ -1,31 +1,19 @@
 @echo off
 
-if EXIST contest_A.cpp goto DEL
-if NOT EXIST contest_A.cpp goto NEW
+set alpha=A B C D E F
 
-:NEW
-copy code.cpp contest_A.cpp
-copy code.cpp contest_B.cpp
-copy code.cpp contest_C.cpp
-copy code.cpp contest_D.cpp
-copy code.cpp contest_E.cpp
-copy code.cpp contest_F.cpp
-exit
-
-:DEL
-del contest_A.cpp
-del contest_B.cpp
-del contest_C.cpp
-del contest_D.cpp
-del contest_E.cpp
-del contest_F.cpp
-del contest_A.exe
-del contest_B.exe
-del contest_C.exe
-del contest_D.exe
-del contest_E.exe
-del contest_F.exe
 del input && del output
 .> input
 .> output
+
+if NOT EXIST contest_A.cpp goto START
+if EXIST contest_A.cpp goto FINISH
+
+:START
+for %%i in (%alpha%) do copy code.cpp contest_%%i.cpp
+exit
+
+:FINISH
+for %%i in (%alpha%) do del contest_%%i.cpp
+for %%i in (%alpha%) do del contest_%%i.exe
 exit
