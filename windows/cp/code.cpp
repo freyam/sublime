b310059434c8665516fbefeb2c9f68f20fd230b0
Freyam Mehta <62539811+freyam@users.noreply.github.com>
2021-01-06 21:39:54 +0400

    Setup v2021.1

--- windows/cp/code.cpp
+++ windows/cp/code.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+typedef long long ll;
+typedef long double ld;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef pair<ll, ll> pl;
+
+#define ff first
+#define ss second
+#define pb push_back
+#define sz(v) ((ll) v.size())
+#define all(v) v.begin(), v.end()
+
+void solve() {
+	
+}
+
+int main() {
+    cin.tie(0), cout.tie(0) -> sync_with_stdio(0);
+    int tc; cin >> tc; while(tc--)
+    solve();
+    return 0;
+}
