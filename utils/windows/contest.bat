f0f7eed5fd922c9f62a8d2a12ab37dae5ce1f854
Freyam Mehta <62539811+freyam@users.noreply.github.com>
2021-03-30 10:54:15 +0400

    Update contest.bat

--- utils/windows/contest.bat
+++ utils/windows/contest.bat
@@ -4,7 +4,7 @@ if NOT EXIST contest_A.cpp goto CREATE
 if EXIST contest_A.cpp goto CLEAN
 
 :CREATE
-for %%i in (A B C D E F) do copy code.cpp contest_%%i.cpp
+for %%i in (A B C D E F) do copy template.cpp contest_%%i.cpp
 .> input.txt
 .> output.txt
 exit
